612b249d1eb5d995ce7b782100a6c7eea58e7177
Jim Crowley <jamescro@microsoft.com>
2016-09-14 15:53:43 -0700

    adding starter project

---
+++ starter/Graph-UWP-Connect-REST/AuthenticationHelper.cs
@@ -0,0 +1,46 @@
+﻿//Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license.
+//See LICENSE in the project root for license information.
+
+using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Security.Authentication.Web;
+using Windows.Security.Authentication.Web.Core;
+using Windows.Security.Credentials;
+using Windows.Storage;
+using Microsoft.Identity.Client;
+
+namespace Graph_UWP_Connect_REST
+{
+    public class AuthenticationHelper
+    {
+        // The Client ID is used by the application to uniquely identify itself to the v2.0 authentication endpoint.
+        static string clientId = App.Current.Resources["ida:ClientID"].ToString();
+        public static string[] Scopes = { "https://graph.microsoft.com/User.Read", "https://graph.microsoft.com/Mail.Send" };
+        public static PublicClientApplication IdentityClientApp = new PublicClientApplication(clientId);
+        public static string TokenForUser = null;
+        public static DateTimeOffset Expiration;
+        public static ApplicationDataContainer _settings = ApplicationData.Current.RoamingSettings;
+
+        /// <summary>
+        /// Get Token for User.
+        /// </summary>
+        /// <returns>Token for user.</returns>
+        public static async Task<string> GetTokenForUserAsync()
+        {
+
+        }
+
+        /// <summary>
+        /// Signs the user out of the service.
+        /// </summary>
+        public static void SignOut()
+        {
+
+
+        }
+
+    }
+}
---
+++ starter/Graph-UWP-Connect-REST/MailHelper.cs
@@ -0,0 +1,33 @@
+﻿//Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license.
+//See LICENSE in the project root for license information.
+
+using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Security.Authentication.Web;
+using Windows.Security.Authentication.Web.Core;
+using Windows.Security.Credentials;
+using Windows.Storage;
+using Microsoft.Identity.Client;
+
+namespace Graph_UWP_Connect_REST
+{
+    class MailHelper
+    {
+        /// <summary>
+        /// Compose and send a new email.
+        /// </summary>
+        /// <param name="subject">The subject line of the email.</param>
+        /// <param name="bodyContent">The body of the email.</param>
+        /// <param name="recipients">A semicolon-separated list of email addresses.</param>
+        /// <returns></returns>
+        internal async Task ComposeAndSendMailAsync(string subject,
+                                                            string bodyContent,
+                                                            string recipients)
+        {
+
+        }
+    }
+}
